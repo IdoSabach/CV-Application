668e549bbaea54b599565dc7460e933db7b466af
Ido <Idosabach11@gmail.com>
2024-02-10 23:18:50 +0200

    fix and add to personal localStorage

--- src/components/MainDesign/PersonalDetails.jsx
+++ src/components/MainDesign/PersonalDetails.jsx
@@ -1,5 +1,5 @@
+import React, { useState, useEffect } from "react";
 import "./PersonalDetails.css";
-import React, { useState } from "react";
 
 export default function PersonalDetails({
   onNameChange,
@@ -8,38 +8,55 @@ export default function PersonalDetails({
   onAddressChange,
 }) {
   const [isOpen, setIsOpen] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
+  const [formData, setFormData] = useState({
+    name: localStorage.getItem("name") || "",
+    email: localStorage.getItem("email") || "",
+    phone: localStorage.getItem("phone") || "",
+    address: localStorage.getItem("address") || "",
+  });
 
   const handleOpen = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleNameChange = (e) => {
-    const newName = e.target.value;
-    setName(newName);
-    onNameChange(newName);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    localStorage.setItem(name, value);
   };
 
-  const handleEmailChange = (e) => {
-    const newEmail = e.target.value;
-    setEmail(newEmail);
-    onEmailChange(newEmail);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onNameChange(formData.name);
+    onEmailChange(formData.email);
+    onPhoneChange(formData.phone);
+    onAddressChange(formData.address);
   };
 
-  const handlePhoneChange = (e) => {
-    const newPhone = e.target.value;
-    setPhone(newPhone);
-    onPhoneChange(newPhone);
+  const handleClear = () => {
+    setFormData({
+      name: "",
+      email: "",
+      phone: "",
+      address: "",
+    });
+    localStorage.removeItem("name");
+    localStorage.removeItem("email");
+    localStorage.removeItem("phone");
+    localStorage.removeItem("address");
   };
 
-  const handleAddressChange = (e) => {
-    const newAddress = e.target.value;
-    setAddress(newAddress);
-    onAddressChange(newAddress);
-  };
+  useEffect(() => {
+    setFormData({
+      name: localStorage.getItem("name") || "",
+      email: localStorage.getItem("email") || "",
+      phone: localStorage.getItem("phone") || "",
+      address: localStorage.getItem("address") || "",
+    });
+  }, []);
 
   return (
     <div id="personalDetails">
@@ -49,48 +66,63 @@ export default function PersonalDetails({
           <span className="material-symbols-outlined">menu_open</span>
         </button>
       </div>
-      {isOpen ? (
-        <form action="">
+      {isOpen && (
+        <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="">Full Name</label>
+            <label htmlFor="name">Full Name</label>
             <input
               type="text"
+              id="name"
+              name="name"
+              required
               placeholder="First and last name"
-              value={name}
-              onChange={handleNameChange}
+              value={formData.name}
+              onChange={handleChange}
             />
           </div>
           <div>
-            <label htmlFor="">Email</label>
+            <label htmlFor="email">Email</label>
             <input
               type="email"
+              id="email"
+              name="email"
               required
               placeholder="Enter email"
-              value={email}
-              onChange={handleEmailChange}
+              value={formData.email}
+              onChange={handleChange}
             />
           </div>
           <div>
-            <label htmlFor="">Phone</label>
+            <label htmlFor="phone">Phone</label>
             <input
               type="text"
+              id="phone"
+              name="phone"
               placeholder="Enter phone number"
-              value={phone}
-              onChange={handlePhoneChange}
+              value={formData.phone}
+              onChange={handleChange}
             />
           </div>
           <div>
-            <label htmlFor="">Address</label>
+            <label htmlFor="address">Address</label>
             <input
               type="text"
+              id="address"
+              name="address"
               placeholder="City, Country"
-              value={address}
-              onChange={handleAddressChange}
+              value={formData.address}
+              onChange={handleChange}
             />
+          </div>
+          <div className="box-btn">
+            <button type="submit" className="save-btn">
+              Save
+            </button>
+            <button onClick={handleClear} type="submit" className="clear-btn">
+              Clear
+            </button>
           </div>
         </form>
-      ) : (
-        ""
       )}
     </div>
   );
