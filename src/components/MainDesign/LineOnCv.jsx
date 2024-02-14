a4dfe36abab0e30f0cd2fd4a1f89499c18a30fec
Ido <Idosabach11@gmail.com>
2024-02-14 23:02:46 +0200

    add block line cv

--- src/components/MainDesign/LineOnCv.jsx
+++ src/components/MainDesign/LineOnCv.jsx
@@ -1,5 +1,20 @@
+import './LineOnCv.css'
+
+
 export default function LineOnCv(){
   return (
-    <></>
+    <div id="line-cv">
+      <div className="left-block">
+        <div className="date">
+          <div className="start-date"></div>
+          <div className="end-date"></div>
+        </div>
+        <div className="location"></div>
+      </div>
+      <div className="right-block">
+        <div className="school"></div>
+        <div className="degree"></div>
+      </div>
+    </div>
   )
 }
